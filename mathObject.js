c3bd69d0b74d849df20c1c37d20c48fbea2d1769
pasyet <farapasyetnabila@gmail.com>
2019-02-13 01:24:27 +0700

    study math objects

---
+++ mathObject.js
@@ -0,0 +1,18 @@
+
+
+
+// create a random number
+var r = Math.random();
+console.log(r);
+
+// return the largest number of 10 and 20
+var x = Math.max(10,20);
+console.log(x);
+
+//round a number to the nearest integer (return 5)
+var y = Math.round(5.3);
+console.log(y);
+
+//get the square root of 9
+var z = Math.sqrt(9);
+console.log(z);
