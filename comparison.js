7da2d4472abe7f4c9964c5186dde190529ea0c99
pasyet <farapasyetnabila@gmail.com>
2019-02-18 04:36:32 +0700

    study comparison

---
+++ comparison.js
@@ -0,0 +1,19 @@
+var x = 5;
+
+console.log(x == 8);      //equal to
+console.log(x == 5);
+console.log(x == "5");
+
+console.log(x === 5);     //equal value
+console.log(x === "5");   //equal type
+
+console.log(x != 8);      //not equal
+
+console.log(x !==5);      //not equal value
+console.log(x !== "5");   //not equal type
+console.log(x !== 8);
+
+console.log(x > 8);       //greater than
+console.log(x < 8);       //less than
+console.log(x >= 8);      //greater than equal
+console.log(x <= 8);      //less than equal
